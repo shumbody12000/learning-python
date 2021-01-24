16eaa57227b8c59e107412372405420acdc7d39e
David Schumm <shumbody12000@yahoo.com>
2021-01-24 12:11:18 -0500

    update

--- test.py
+++ test.py
@@ -3,17 +3,41 @@
 #David
 ###########
 
-
-
+#clear the terminal screen
 import os
 os .system('clear')
 
+#######################
+#DATA TYPES
+#Strings
+#Numbers
+#Lists
+#Tuples
+#Dictionaries
+#Boolean
+#######################
 
-full_name = "John Elder"
-age = 41
-name = ["john", "bob", "mary"]
+num = 1
+fizzcount = 0
+buzzcount = 0
+fizzbuzzcount = 0
 
+while (num <= 100):
+	if(num % 3 == 0) and (num % 5 == 0):
+		print (str(num) + ". fizzbuzz")
+		fizzbuzzcount += 1
 
-# print hello world to the screen
-print (name[0])
+	elif (num % 3 == 0):
+		print(str(num) + ". fizz")
+		fizzcount += 1
+	
+	elif (num % 5 == 0):
+		print(str(num) + ". buzz")
+		buzzcount += 1
+	else:
+		print(num)
+	num +=1	
 
+print ("__________________________")
+print ("fizz\tbuzz\tfizzbuzz")
+print (str(fizzcount) + "\t" + str(buzzcount) + "\t" + str(fizzbuzzcount))
